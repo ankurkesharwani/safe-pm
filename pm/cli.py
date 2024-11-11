a952f67b0bea770f432858e87ec9bc1cec813086
Ankur Kesharwani <ankur.kesharwani001@gmail.com>
2024-11-11 23:51:47 +0530

    Restructure directories

---
+++ pm/cli.py
@@ -0,0 +1,32 @@
+import argparse
+
+def cli_start():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.version:
+        print(0.1)
+        return 0
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog="safe-pm",
+                                     description="SafePM: A secure, simple, open-source password manager.")
+    parser.add_argument("-v", "--version", action="store_true", help="Print version")
+    program_subparser = parser.add_subparsers(dest="program", title="program", metavar="<program>", required=True)
+
+    # Attach program subparsers
+    attach_setup_subparser(program_subparser)
+
+    return parser
+
+
+def attach_setup_subparser(program_subparser):
+    description = "Create a new database for storing passwords"
+    setup_program_parser = program_subparser.add_parser("setup", description=description, help=description.lower())
+    setup_program_parser.add_argument("--db", type=str, help="name of the database to create", required=True)
+    setup_program_parser.set_defaults(func=setup_safe)
+
+
+def setup_safe(args):
+    print(args)
