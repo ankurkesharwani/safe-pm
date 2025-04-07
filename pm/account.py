95d2c8c02f9d6b615de0d87bcee5f5a92187c68c
Ankur Kesharwani <ankur.kesharwani001@gmail.com>
2025-04-07 22:59:15 +0530

    Updated styling and documentation comments

--- pm/account.py
+++ pm/account.py
@@ -15,41 +15,45 @@ class AccountException(Exception):
 
 def list_accounts(args):
     try:
+        # Read input params
         db_path = get_db_path()
         db = args.db
         store = args.store
 
+        # Verify account credentials
         password = getpass.getpass("Enter password:")
         if not verify_password(password, db):
             raise AccountException("Error: [Account] - Entered password is incorrect")
-
         if not file_exists_in_path(db_path, db):
             raise AccountException(f"Error: [Account] - The requested db with name {db} does not exist")
 
+        # Listing accounts
         store_hid = get_deterministic_hash(store)
         encryption_key = derive_encryption_key(password)
-
         with sqlite3.connect(os.path.join(db_path, db)) as connection:
             cursor = connection.cursor()
             try:
+                # Get store id from db
                 db_result = cursor.execute(f"SELECT id FROM store WHERE hid='{store_hid}'")
                 store_id_record = db_result.fetchone()
                 if store_id_record is None:
                     raise DatabaseException("Error: [Database] - Store does not exists.")
                 store_id = store_id_record[0]
 
+                # Fetch account records from db
                 account_db_result = cursor.execute(f"SELECT * FROM account WHERE store_id='{store_id}'")
                 account_records = account_db_result.fetchall()
                 output = []
                 for r in account_records:
                     output.append(
-                        (decrypt(r[2], encryption_key), decrypt(r[3], encryption_key), decrypt(r[4], encryption_key),
-                         r[6])
+                        (
+                            decrypt(r[2], encryption_key), decrypt(r[3], encryption_key), decrypt(r[4], encryption_key),
+                            r[6]
+                        )
                     )
 
-                display_table_in_less_with_ansi(
-                    header=("Name", "Username", "Email", "Created At"), rows=output
-                )
+                # Display in Less
+                display_table_in_less_with_ansi(header=("Name", "Username", "Email", "Created At"), rows=output)
             except Exception as e:
                 raise DatabaseException(f"Error: [Database] - {str(e)}")
             finally:
@@ -61,6 +65,7 @@ def list_accounts(args):
 
 def create_account(args):
     try:
+        # Read input params
         db_path = get_db_path()
         db = args.db
         store = args.store
@@ -75,9 +80,9 @@ def create_account(args):
         use_no_digits = args.pass_no_digits
         exclude_chars = args.pass_exclude_chars
 
-        # Get the password to save
-        selected_password = None
+        # Configure password to save
         if set_user_password:
+            # Get password from user
             selected_password = getpass.getpass("Enter password to save for this account:")
 
             # Generate a rainbow table
@@ -86,19 +91,21 @@ def create_account(args):
                 for line in file:
                     rainbow_table.append(line.strip())
 
+            # Compute password strength
             strength = calculate_password_strength(selected_password)
             rainbow_match, distance = find_most_similar_password(selected_password, rainbow_table)
             print(f"You have chosen a password with strength: {strength}")
             if rainbow_match is not None and distance < 5:
                 print(f"Your chosen password is very similar to a dictionary password '{rainbow_match}'")
         elif set_auto_gen_password:
+            # Auto generate password
             selected_password = generate_random_password(
                 password_min_length, password_max_length, use_no_special_chars, use_no_digits, exclude_chars
             )
         else:
             raise AccountException("Error: [Account] - Password to set cannot be empty.")
 
-        # Confirm store password
+        # Verify account credentials
         password = getpass.getpass("Enter password:")
         if not verify_password(password, db):
             raise AccountException("Error: [Account] - Entered password is incorrect")
@@ -106,7 +113,7 @@ def create_account(args):
         if not file_exists_in_path(db_path, db):
             raise AccountException(f"Error: [Account] - The requested db with name {db} does not exist")
 
-        # Encrypt values to save
+        # Create account
         encryption_key = derive_encryption_key(password)
         store_hid = get_deterministic_hash(store)
         account_hid = get_deterministic_hash(account)
@@ -114,12 +121,10 @@ def create_account(args):
         encrypted_password_to_save = encrypt(selected_password, encryption_key)
         encrypted_username = encrypt(username, encryption_key) if username is not None else encrypt("", encryption_key)
         encrypted_email = encrypt(email, encryption_key) if email is not None else encrypt("", encryption_key)
-
-        # Same in db
         with sqlite3.connect(os.path.join(db_path, db)) as connection:
             cursor = connection.cursor()
             try:
-                # Get the store id
+                # Get store id from db
                 db_result = cursor.execute(f"SELECT id FROM store WHERE hid='{store_hid}'")
                 store_id_record = db_result.fetchone()
                 if store_id_record is None:
@@ -127,6 +132,7 @@ def create_account(args):
                 store_id = store_id_record[0]
 
                 # Save account and password
+                cursor.execute("BEGIN TRANSACTION")
                 cursor.execute(
                     f"INSERT INTO account (hid, name, username, email, store_id) "
                     f"VALUES ("
@@ -140,8 +146,6 @@ def create_account(args):
                     f"  '{account_id}', '{encrypted_password_to_save}'"
                     f")"
                 )
-
-                # Commit db
                 connection.commit()
             except sqlite3.IntegrityError as e:
                 raise AccountException(
@@ -153,7 +157,6 @@ def create_account(args):
             finally:
                 if not cursor:
                     cursor.close()
-
     except AccountException as e:
         raise e
     except Exception as e:
@@ -162,33 +165,34 @@ def create_account(args):
 
 def view_account_credentials(args):
     try:
+        # Read input params
         db_path = get_db_path()
         db = args.db
         store = args.store
         account = args.account
 
-        # Confirm store password
+        # Verify account credentials
         password = getpass.getpass("Enter password:")
         if not verify_password(password, db):
             raise AccountException("Error: [Account] - Entered password is incorrect")
-
         if not file_exists_in_path(db_path, db):
             raise AccountException(f"Error: [Account] - The requested db with name {db} does not exist")
 
+        # View account credentials
         encryption_key = derive_encryption_key(password)
         store_hid = get_deterministic_hash(store)
         account_hid = get_deterministic_hash(account)
-
         with sqlite3.connect(os.path.join(db_path, db)) as connection:
             cursor = connection.cursor()
             try:
-                # Get the store id
+                # Get store id from db
                 db_result = cursor.execute(f"SELECT id FROM store WHERE hid='{store_hid}'")
                 store_id_record = db_result.fetchone()
                 if store_id_record is None:
                     raise DatabaseException("Error: [Database] - Store does not exists.")
                 store_id = store_id_record[0]
 
+                # Get account credentials from db
                 account_db_result = cursor.execute(
                     f"SELECT * FROM account "
                     f"  LEFT JOIN password ON account.id=password.id "
@@ -199,16 +203,18 @@ def view_account_credentials(args):
                 output = []
                 for r in account_records:
                     output.append(
-                        (decrypt(r[2], encryption_key), decrypt(r[3], encryption_key), decrypt(r[4], encryption_key),
-                         decrypt(r[9], encryption_key), r[10])
+                        (
+                            decrypt(r[2], encryption_key), decrypt(r[3], encryption_key), decrypt(r[4], encryption_key),
+                            decrypt(r[9], encryption_key), r[10]
+                        )
                     )
+
+                # Display in Less
                 display_table_in_less_with_ansi(
                     header=("Name", "Username", "Email", "Password", "Created At"), rows=output
                 )
-
             except Exception as e:
                 raise DatabaseException("Error: [Database] - Could not get account in the store.")
-
     except AccountException as e:
         raise e
     except Exception as e:
